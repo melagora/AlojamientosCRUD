661028d69fbcdea50461031d42d6e1516e44d68d
Adalid2003 <68620037+Adalid2003@users.noreply.github.com>
2025-01-07 19:55:46 -0600

    cambios en header

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,7 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<?php
+// Verificar si el usuario está autenticado
+if (!isset($_SESSION['user_id'])) {
+    echo '<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container">
         <a class="navbar-brand" href="#"><img src="../assets/images/El cuche.png" alt="Logo" style="height: 40px;"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,10 +14,40 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="../views/alojamientos.php">Alojamientos</a>
+                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="../views/cuenta_usuario.php">Cuenta de Usuario</a>
                 </li>
-                 <li class="nav-item">
+                <li class="nav-item">
+                    <a class="nav-link" href="../views/contacto.php">Contacto</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Iniciar Sesión</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>';
+ 
+}else{
+    echo '<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+        <a class="navbar-brand" href="#"><img src="../assets/images/El cuche.png" alt="Logo" style="height: 40px;"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="../views/landing_page.php">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../views/alojamientos.php">Alojamientos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../views/cuenta_usuario.php">Cuenta de Usuario</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="../views/contacto.php">Contacto</a>
                 </li>
                 <li class="nav-item">
@@ -23,4 +56,7 @@
             </ul>
         </div>
     </div>
-</nav>
+</nav>';
+
+}
+?>
